a25adbf8ed24b1492a68074d82717af28f59f492
ilya <bersek123@gmail.com>
2022-01-26 16:00:39 +0300

    123

--- airflow.sh
+++ airflow.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+docker-compose down
 sudo mkdir -p ./dags ./logs ./plugins
 sudo chmod -R 777 logs/ plugins/ dags/ airflow.cfg
-echo -e "AIRFLOW_UID=$(id -u) \nAIRFLOW_GID=0" > .env
+echo -e "AIRFLOW_UID=50000\nAIRFLOW_GID=0" > .env
+# echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env
 docker-compose up airflow-init
 docker-compose up -d
