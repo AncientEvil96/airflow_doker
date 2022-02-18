34d4c09ab95ff8530ec181bb33a15e647b2f07bf
ilya <bersek123@gmail.com>
2022-02-18 17:35:49 +0300

    123

--- project/for_netcat_circul_tbp_ght/e_subdivision_tbp.py
+++ project/for_netcat_circul_tbp_ght/e_subdivision_tbp.py
@@ -14,24 +14,35 @@ if __name__ == '__main__':
     )
 
     query = """
-    SELECT CONVERT(bigint, [_Fld395])                                     AS TBP_ID
-         , [_Reference505]._Description                                    AS Subdivision_Name
-         , CONVERT(bigint, [_Fld396])                                      AS parent_id
-         , 1                                                               AS Catalogue_ID
-    --      , 'category-' + CONVERT(nvarchar, [_Fld395])                      AS EnglishName
-    --      , 0                                                               AS Checked
-    --      , CONVERT(bigint, [_Fld395])                                      AS Priority
-         , -1                                                              AS DisallowIndexing
-         , -1                                                              AS IncludeInSitemap
-    --      , 1                                                               AS UseMultiSubClass
-         , [_InfoRg393].[_Fld720]                                          AS menu_pic
-    --      , '' + [_Reference505]._Description + N'| сеть магазинов "ВПРОК"' AS Title
-         , 2                                                               AS Template_ID
-    --      , CONVERT(tinyint, [_Fld1003])                                    AS hide_tbp
-    FROM [TBP_WORK].[dbo].[_InfoRg393]
+    select CONVERT(bigint, [_Fld395])               AS TBP_ID
+         , [_Reference505]._Description             AS Subdivision_Name
+         , CONVERT(bigint, [_Fld396])               AS parent_id
+         , IIF(PVC._Description = N'ЦИРКУЛЬ', 2, 1) AS Catalogue_ID
+         , [_InfoRg393].[_Fld720]                   AS menu_pic
+    from [TBP_WORK].[dbo]._Reference1145
+             INNER JOIN (select _IDRRef, _Description
+                         from [TBP_WORK].[dbo]._Reference1145
+                         WHERE _Description in (N'ЦИРКУЛЬ', N'ВПРОК')) as PVC
+                        on _Reference1145._ParentIDRRef = PVC._IDRRef
+             INNER JOIN [TBP_WORK].[dbo]._InfoRg1149
+                        on _Fld1162 = _Fld1152
+             INNER JOIN [TBP_WORK].[dbo]._Reference11
+                        ON _Reference11._Code = _InfoRg1149._Fld1150
+                            AND _Reference11._Marked = 0
+                            AND _Reference11.[_Fld1165] IN (1, 2, 3, 4)
+             INNER JOIN [TBP_WORK].[dbo]._InfoRg406
+                        ON _Fld407RRef = _Reference11._IDRRef
              INNER JOIN [TBP_WORK].[dbo]._Reference505 as _Reference505
-                        ON _Fld394RRef = _Reference505._IDRRef
-    WHERE [_Fld508] = 1 AND [_Fld1003] = 0;
+                        ON _Fld731RRef = _Reference505._IDRRef
+             INNER JOIN [TBP_WORK].[dbo].[_InfoRg393] ON _Fld394RRef = _Reference505._IDRRef
+    WHERE _Reference1145._Marked = 0
+    GROUP BY
+           CONVERT(bigint, [_Fld395])
+         , [_Reference505]._Description
+         , CONVERT(bigint, [_Fld396])
+         , IIF(PVC._Description = N'ЦИРКУЛЬ', 2, 1)
+         , [_InfoRg393].[_Fld720]
+    ;
     """
 
     source.select_to_parquet(query, '/tmp/tmp/subdivision')
--- project/for_netcat_circul_tbp_ght/l_subdivision_vprok.py
+++ project/for_netcat_circul_tbp_ght/l_subdivision_vprok.py
@@ -7,30 +7,45 @@ host, port, password, login, database, file = argv[1:]
 
 def get_load_list():
     df = pd.read_parquet(file)
-    next_step = [9, 8, 7, 8225]
-    df2 = df.loc[df['TBP_ID'].isin(next_step)]
-    count = 0
-    while True:
-        df3 = df.loc[df['parent_id'].isin(next_step)]
-        df2 = pd.concat([df2, df3])
-        if count == len(df2):
-            break
-        count = len(df2)
-        next_step = df3['TBP_ID']
+    df.loc[df['Catalogue_ID'] == 2, 'Template_ID'] = 5
+    df.loc[df['Catalogue_ID'] == 1, 'Template_ID'] = 2
+    df.loc[
+        (df['parent_id'] == 0) & (df['Catalogue_ID'] == 2),
+        ['Parent_Sub_ID', 'DisallowIndexing', 'IncludeInSitemap']
+    ] = (52, 1, 1)
+    df.loc[
+        (df['parent_id'] == 0) & (df['Catalogue_ID'] == 1),
+        ['Parent_Sub_ID', 'DisallowIndexing', 'IncludeInSitemap']
+    ] = (421, 1, 1)
+    df['Parent_Sub_ID'] = df['Parent_Sub_ID'].fillna(0)
+    df[['DisallowIndexing', 'IncludeInSitemap']] = df[['DisallowIndexing', 'IncludeInSitemap']].fillna(1)
 
-    df2.loc[df2['Catalogue_ID'] == 1, ['Catalogue_ID', 'Template_ID', 'Parent_Sub_ID']] = (2, 5, 52)
-    df2['Title'] = df2['Subdivision_Name'].apply(lambda x: f'{x} | в сети "Циркуль"')
+    # df.loc[df['Catalogue_ID'] == 2, 'Title'] = df['Subdivision_Name'] + ' | в сети "Циркуль"'
+    # df.loc[df['Catalogue_ID'] == 1, 'Title'] = df['Subdivision_Name'] + ' | сеть магазинов "ВПРОК"'
 
-    df['Title'] = df['Subdivision_Name'].apply(lambda x: f'{x} | сеть магазинов "ВПРОК"')
-    df.loc[df['Catalogue_ID'] == 1, 'Parent_Sub_ID'] = 421
-    df = pd.concat([df, df2])
-    df['Parent_Sub_ID'] = df['Parent_Sub_ID'].astype('int')
+    # df2 = df.loc[df['Catalogue_ID'] == 1]
+    # df2['Title'] = df2['Subdivision_Name'] + ' | в сети "Циркуль"'
+    # df3 = df.loc[df['Catalogue_ID'] == 2]
+    # df3['Title'] = df3['Subdivision_Name'] + ' | сеть магазинов "ВПРОК"'
+    # df = pd.concat([df2, df3])
+
+    # df['Title'] = df['Subdivision_Name'].apply(lambda x: f'{x} | в сети "Циркуль"')
+    # df['Title'] = df['Subdivision_Name'].apply(lambda x: f'{x} | сеть магазинов "ВПРОК"')
+    df[['Parent_Sub_ID', 'DisallowIndexing', 'IncludeInSitemap']] = df[
+        ['Parent_Sub_ID', 'DisallowIndexing', 'IncludeInSitemap']].astype('int')
+
+    # print(df.dtypes)
+    # print(df)
+    # print(list(df.itertuples(index=False, name=None))[:])
 
     return list(df.itertuples(index=False, name=None))
 
 
 if __name__ == '__main__':
     load_list = get_load_list()
+
+    # for i in load_list:
+    #     print(i)
 
     target = MySQL(
         params={
@@ -42,11 +57,13 @@ if __name__ == '__main__':
         }
     )
 
+    table = 'aaaaa_test'
+
     target.init_connection()
-    target.query_to_base('drop table if exists aaaaa_test;')
+    target.query_to_base(f'drop table if exists {table};')
     target.query_to_base(
-        """
-        create table `aaaaa_test`
+        f"""
+        create table {table}
             (
                 TBP_ID           int                                              not null,
                 Subdivision_Name varchar(255) default ''                          not null,
@@ -58,26 +75,104 @@ if __name__ == '__main__':
                 DisallowIndexing int          default -1                          null,
                 IncludeInSitemap int          default -1                          null,
                 menu_pic         char(255)                                        null,
-                Title            varchar(255)                                     null,
-                Template_ID      int                                              null
-            );
+                Title            varchar(255) default IF(Catalogue_ID = 1, concat(EnglishName, ' | сеть магазинов "ВПРОК"'),
+                                             concat(EnglishName, ' | в сети "Циркуль"')) null,
+                Template_ID int          null
+        );
         """
     )
 
-    query = """
-            INSERT INTO aaaaa_test
-                (TBP_ID,
-                 Subdivision_Name,
-                 parent_id,
-                 Catalogue_ID,
-                 DisallowIndexing,
-                 IncludeInSitemap,
-                 menu_pic,
-                 Template_ID,
-                 Title,
-                 Parent_Sub_ID)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+    query = f"""
+            INSERT INTO {table}
+                (
+                    TBP_ID          ,
+                    Subdivision_Name,
+                    parent_id       ,
+                    Catalogue_ID    ,
+                    menu_pic        ,
+                    Template_ID     ,
+                    Parent_Sub_ID   ,
+                    DisallowIndexing,
+                    IncludeInSitemap
+                 )
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);
         """
 
     target.load_many_to_base(query, load_list)
 
+    # удаление ненужных данных
+    target.query_to_base(
+        f"""
+        delete
+        from Subdivision
+        where Subdivision.TBP_ID > 0
+          and not Subdivision_ID in (select Subdivision_ID
+                                                  from (select MAX(Subdivision_ID) as Subdivision_ID
+                                                        FROM Subdivision
+                                                                 left join {table} as tt1 on tt1.TBP_ID = Subdivision.TBP_ID
+                                                        where tt1.TBP_ID in
+                                                              (select distinct Subdivision.TBP_ID from Subdivision)
+                                                        group by Subdivision.TBP_ID, Subdivision.Catalogue_ID) as new);
+        """
+    )
+
+    # exit(0)
+
+    # создаем новые если такие есть
+    target.query_to_base(
+        f"""
+            insert into Subdivision (Subdivision_Name,
+                                     EnglishName,
+                                     Parent_Sub_ID,
+                                     Catalogue_ID,
+                                     Priority,
+                                     menu_pic,
+                                     Title,
+                                     TBP_ID,
+                                     Template_ID,
+                                     DisallowIndexing,
+                                     IncludeInSitemap,
+                                     Hidden_URL)
+            select tt1.Subdivision_Name                                        as Subdivision_Name,
+                   tt1.EnglishName                                             as EnglishName,
+                   tt1.Parent_Sub_ID                                           as Parent_Sub_ID,
+                   tt1.Catalogue_ID                                            as Catalogue_ID,
+                   tt1.TBP_ID                                                  as Priority,
+                   tt1.menu_pic                                                as menu_pic,
+                   tt1.Title                                                   as Title,
+                   tt1.TBP_ID                                                  as TBP_ID,
+                   tt1.Template_ID                                             as Template_ID,
+                   IF(tt1.parent_id = 0, 1, tt1.DisallowIndexing)       as DisallowIndexing,
+                   IF(tt1.parent_id = 0, 1, tt1.IncludeInSitemap)       as IncludeInSitemap,
+                   tt1.TBP_ID as Hidden_URL
+            from {table} as tt1
+                     LEFT JOIN Subdivision
+                               on tt1.TBP_ID = Subdivision.TBP_ID
+                                   AND tt1.Catalogue_ID = Subdivision.Catalogue_ID
+                                   and tt1.Catalogue_ID in (1, 2)
+                     LEFT JOIN Subdivision as Parent
+                               on tt1.parent_id = Parent.TBP_ID
+                               and tt1.Catalogue_ID in (1, 2)
+            where Subdivision.Subdivision_ID is null
+            group by tt1.Subdivision_Name                                  ,
+                   tt1.EnglishName                                         ,
+                   tt1.Parent_Sub_ID                                       ,
+                   tt1.Catalogue_ID                                        ,
+                   tt1.TBP_ID                                              ,
+                   tt1.menu_pic                                            ,
+                   tt1.Title                                               ,
+                   tt1.TBP_ID                                              ,
+                   tt1.Template_ID                                         ,
+                   IF(tt1.parent_id = 0, 1, tt1.DisallowIndexing)   ,
+                   IF(tt1.parent_id = 0, 1, tt1.IncludeInSitemap)
+            order by TBP_ID
+            ;
+        """
+    )
+
+    # проверка + обновление данных
+    target.query_to_base(
+        """
+            
+        """
+    )
