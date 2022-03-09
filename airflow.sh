3317bc2b20f8baefb92201749492276286e11294
ilya <bersek123@gmail.com>
2022-03-09 09:44:30 +0300

    three task full is refactoring for docker operations

--- airflow.sh
+++ airflow.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 docker-compose down
-sudo mkdir -p ./dags ./logs ./plugins
-sudo chmod -R 777 logs/ plugins/ dags/ airflow.cfg
-#echo -e "AIRFLOW_UID=50000\nAIRFLOW_GID=0" > .env
- echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env
+sudo mkdir -p ./dags ./logs ./plugins ./project ./tmp
+sudo chmod -R 777 ./dags ./logs ./plugins ./project ./tmp airflow.cfg
+echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env
 docker-compose up airflow-init
 docker-compose up -d
+cd DI
+docker build -t airflow_task_python_3.8 .
